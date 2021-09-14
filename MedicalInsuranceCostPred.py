80f8d3f289ab424f31817ab6d3a7f7dab7eef3ef
Ishant Thakare <ishantrog752@gmail.com>
2021-09-14 12:29:03 +0530

    Update MedicalInsuranceCostPred.py

--- MedicalInsuranceCostPred.py
+++ MedicalInsuranceCostPred.py
@@ -1,7 +1,5 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 
 insurance_dataset = pd.read_csv('data/insurance.csv')
